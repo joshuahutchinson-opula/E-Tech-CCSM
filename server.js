1139b2b708677bdbc8f22d40942db72d462f865d
joshuahutchinson-opula <joshua.ls.hutchinson@gmail.com>
2026-06-25 14:49:57 -0500

    Set trust proxy for Express app

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const helmet = require('helmet');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1); 
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || 'ccsm-super-secret-key-2026';
 
