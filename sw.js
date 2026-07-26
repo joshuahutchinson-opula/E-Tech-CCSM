23f55b2724811b41a5f9216db7192b755f61c98c
joshuahutchinson-opula <joshua.ls.hutchinson@gmail.com>
2026-07-26 14:20:56 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'cams-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/field-app.html',
+  '/logo.png',
+  '/favicon.png',
+  '/particles.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
+    })
+  );
+});
